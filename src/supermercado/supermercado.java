bf365e87212c61e6ae1256e6216ca7571780b975
Sebastian <sebastian13200000@gmail.com>
2024-03-01 15:02:40 -0600

    Tercer commit

--- src/supermercado/supermercado.java
+++ src/supermercado/supermercado.java
@@ -4,6 +4,16 @@
  */
 package supermercado;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Asus
@@ -29,6 +39,10 @@ public class supermercado extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTree1 = new javax.swing.JTree();
         jMenuBar1 = new javax.swing.JMenuBar();
         File = new javax.swing.JMenu();
         Window = new javax.swing.JMenu();
@@ -40,7 +54,87 @@ public class supermercado extends javax.swing.JFrame {
         jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 800, 40));
 
         jButton1.setText("ENTER");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
         jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 20, 100, 40));
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Id", "Name", "Category", "Price", "Aisle", "Bin"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.Double.class, java.lang.Integer.class, java.lang.Integer.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 80, 720, 490));
+
+        javax.swing.tree.DefaultMutableTreeNode treeNode1 = new javax.swing.tree.DefaultMutableTreeNode("Archivos CSV");
+        jTree1.setModel(new javax.swing.tree.DefaultTreeModel(treeNode1));
+        jScrollPane2.setViewportView(jTree1);
+
+        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 83, 190, 450));
 
         File.setText("File");
         jMenuBar1.add(File);
@@ -66,6 +160,23 @@ public class supermercado extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+       String []token=jTextField1.getText().split(" ");
+        if (jTextField1.getText().equals("")) {
+            JOptionPane.showMessageDialog(null, "No ha ingresado nada");
+        } else {
+            if(token[0].equals("./create")&& token[1].contains(".txt")&&token[2].equals("-single")){
+                try {
+                    crearArchivo(jTextField1.getText());
+                } catch (IOException ex) {
+                    Logger.getLogger(supermercado.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }else{
+                JOptionPane.showMessageDialog(null, "Comnado no valido");
+            }
+        }
+    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -102,6 +213,37 @@ public class supermercado extends javax.swing.JFrame {
         });
     }
 
+    public void crearArchivo(String comando) throws IOException {
+        String[] token = comando.split(" ");
+        DefaultTableModel modelo = (DefaultTableModel) jTable1.getModel();
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        if (jTextField1.getText().equals("")) {
+            JOptionPane.showMessageDialog(null, "Ingrese el comando para la creacion de archivos");
+
+        } else {
+            fw = new FileWriter("./"+token[1],false);
+            bw = new BufferedWriter(fw);
+            for (int i = 0; i < modelo.getRowCount(); i++) {
+                
+                if (modelo.getValueAt(i, 0)!=null) {
+                    bw.write(modelo.getValueAt(i, 0) + ",");
+                    bw.write(modelo.getValueAt(i, 1) + ",");
+                    bw.write(modelo.getValueAt(i, 3) + ",");
+                    bw.write(modelo.getValueAt(i, 4) + ",");
+                    bw.write(modelo.getValueAt(i, 5) + "\n");
+                } else {
+                    break; 
+                }
+            }
+            bw.flush();
+            bw.close();
+            fw.close();
+        }
+
+    }
+
+    File archivo;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenu File;
     private javax.swing.JMenu Help;
@@ -109,6 +251,10 @@ public class supermercado extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTree jTree1;
     // End of variables declaration//GEN-END:variables
 }
